e0d09c9b08c7915d276703b70dd487e762d2c20c
Los Angeles <los@angeles.pw>
2018-08-30 18:39:44 +0800

    sec: first commit in week

---
+++ c/second-week/fathm_ft.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int feet, fathoms;
+
+  fathoms = 2;
+  feet = 6;
+  printf("There are %d feet in %d fathoms! \n", feet, fathoms);
+  printf("Yes, I said %d feet!\n", fathoms);
+
+  return 0;
+}
